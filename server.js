07dfcbed91b6a2a0711ab475433c1e47bb6946a2
anthony-baru <anthonybaru@gmail.com>
2020-03-16 11:25:16 +0300

    init

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//loading models
+const db = require("./models");
+const Role = db.role;
+
+//for production
+db.sequelize.sync();
+//for dev
+// db.sequelize.sync().then(() => {
+//     console.log('Drop and Resync Db');
+//     initial();
+// });
+// function initial() {
+//     Role.create({
+//         id: 1,
+//         name: "user"
+//     });
+
+//     Role.create({
+//         id: 2,
+//         name: "moderator"
+//     });
+
+//     Role.create({
+//         id: 3,
+//         name: "admin"
+//     });
+// }
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to apa claim application." });
+});
+
+// routes
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
